029d28cd451171b2ce7a8050a0d685f9db6250a7
davidmschy <davidmschy@gmail.com>
2026-02-24 16:28:43 -0800

    Add genii_call_list/config/desktop.py

---
+++ genii_call_list/config/desktop.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "Genii Call List Management",
+            "color": "#E94560",
+            "icon": "octicon octicon-device-mobile",
+            "type": "module",
+            "label": _("Genii Call List")
+        }
+    ]
